74f852a02ac15f06fc94153bad65063d806e8847
Nandeep2750 <barochiya.barochiya@gmail.com>
2024-05-11 17:07:04 +0530

    Feat: Route configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 
 @Component({
   standalone: true,
   selector: "app-root",
-  imports: [HomeComponent],
-  template: `
-    <main>
-      <header class="header">
-        <img
-          class="brand-logo"
-          src="/assets/logo.svg"
-          alt="logo"
-          area-hidden="true"
-        />
-      </header>
-      <section class="main-section">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+  imports: [HomeComponent, RouterModule],
+  templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
