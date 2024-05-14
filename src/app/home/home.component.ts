ed317cc0eaabef9091e008ae8c6dbddeb45bf95a
Nandeep2750 <barochiya.barochiya@gmail.com>
2024-05-14 15:46:58 +0530

    Feat: Implement filter feature.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,11 +13,24 @@ import { HousingService } from './../housing.service';
 })
 export class HomeComponent {
   housingLocations: HousingLocation[] = [];
+  filteredLocations: HousingLocation[] = [];
+  allCities: string[] = [];
   housingService: HousingService = inject(HousingService);
+  selectedCity: string = '';
 
   constructor() {
     this.housingService.getAllHousingLocations().then((housingLocations: HousingLocation[]) => {
       this.housingLocations = housingLocations;
+      this.filteredLocations = housingLocations;
+      this.allCities = Array.from(new Set(this.housingLocations.map(location => location.city)));
+    });
+  }
+
+  onOptionChange(event: Event) {
+    const selectElement = event?.target as HTMLSelectElement;
+    const selectedCity = selectElement.value;
+    this.filteredLocations = this.housingLocations.filter((housingLocation: HousingLocation) => {
+      return housingLocation.city.toLowerCase().includes(selectedCity.toLowerCase());
     });
   }
 }
